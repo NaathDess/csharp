7714fcccff9b2fb4c2bc4e2fb3a068d8ed1a9a0a
NaathDess <nathalia.abade28@gmail.com>
2024-04-02 16:45:58 -0400

    add classe Funcionario

--- csharp/Pessoa.cs
+++ csharp/Pessoa.cs
@@ -34,3 +34,18 @@ public class Estudante : Pessoa{
          set { curso = value;}
     }
 }
+
+public class Funcionario : Pessoa {
+    private double salario:
+
+    public double Bonus(){
+        return salario *0.1;
+    }
+    public Funcionario(string nome, int idade, string cpf, double salario): base(nome, idade, cpf){
+        salario = salario:
+    }
+    public string Salario {
+        get { return salario;}
+        set { salario = value;}
+    }
+}
