909ca492ebd649183e05b49cebe9dad0c2b76174
NaathDess <nathalia.abade28@gmail.com>
2024-04-02 16:32:55 -0400

    add classe pessoa

---
+++ csharp/Pessoa.cs
@@ -0,0 +1,25 @@
+public class Pessoa {
+    private String nome:
+      private int idade:
+         private String cpf:
+
+         public Pessoa(string nome. int idade. string cpf){
+            Nome = nome:
+            Idade = idade:
+            CPF = cpf:
+         }
+
+         public string Nome {
+            get { return nome;}
+            set { nome = value;}
+         }
+         public int Idade {
+            get { return idade;}
+            set { idade = value;}
+         }
+          public int CPF {
+            get { return cpf;}
+            set { cpf = value;}
+          }
+}
+
